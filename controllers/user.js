4274718fcbd25c560d5fcf2775dbd316792cbb2d
Justin Stowe <justinstowe12@gmail.com>
2023-05-20 10:36:48 -0600

    good progress made on user controller, more work required.

---
+++ controllers/user.js
@@ -0,0 +1,78 @@
+const User = require("../models/user");
+const chalk = require("chalk");
+//const Post = require('../models/post')
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
+const { v4: uuidv4 } = require("uuid");
+const S3 = require("aws-sdk/clients/s3");
+const s3 = new S3();
+
+module.exports = {
+  signUp,
+  login,
+  //profile,
+  //updateBio,
+};
+
+async function updateBio(req, res) {
+  try {
+    const newProfile = await User.findOneAndUpdate(
+      { _id: req.user._id },
+      { bio: req.body.bio },
+      { new: true }
+    );
+    console.log(chalk.blueBright("new profile:"), chalk.green(newProfile));
+    res.status(200).json({ data: newProfile });
+  } catch (error) {
+    console.error(chalk.redBright("updateBio error:"), chalk.red(error));
+    res.status(400).json({ error });
+  }
+}
+
+async function profile(req, res) {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    if (!user) return res.status(400).json({ err: "User not found" });
+
+    const posts = await postMessage
+      .find({ user: user._id })
+      .populate("user")
+      .exec();
+    console.log(chalk.blue("user posts"), chalk.blueBright(posts));
+    res.status(200).json({ posts: posts, user: user });
+  } catch (error) {
+    console.error(chalk.red("profile error"), chalk.redBright(error));
+    res.status(400).json({ error });
+  }
+}
+
+async function signup(req, res) {
+  console.log(
+    chalk.green("req.body:"),
+    chalk.blue(req.body),
+    chalk.green("req file:"),
+    chalk.blue(req.file)
+  );
+
+  const filePath = `${uuidv4()}/${req.file.originalname}`;
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: filePath,
+    Body: req.file.buffer,
+  };
+
+  s3.upload(params, async function (err, data) {
+    //data is from successful response from aws, found in data.location
+    console.error(chalk.red("error from aws"), chalk.redBright(err));
+
+    const user = new User({ ...req.body, photoUrl: data.location });
+    try {
+      await user.save();
+      const token = createJWT(user);
+      res.json({ token });
+    } catch (error) {
+      console.error(chalk.red("sign-up error"), chalk.redBright(error));
+      res.status(400).json({ error });
+    }
+  });
+}
