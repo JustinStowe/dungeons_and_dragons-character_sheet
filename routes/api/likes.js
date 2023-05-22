7489614904d9d2518fa62cd4d26702133f37daac
Justin Stowe <justinstowe12@gmail.com>
2023-05-22 13:52:04 -0600

    created posts router.

--- routes/api/likes.js
+++ routes/api/likes.js
@@ -4,3 +4,5 @@ const likesCtrl = require("../../controllers/likes");
 
 router.post("/posts/:id/likes", likesCtrl.create);
 router.delete("/likes/:id", likesCtrl.deleteLike);
+
+module.exports = router;
