286e1787efeb83742f459df906a0f2ffeb531add
Justin Stowe <justinstowe12@gmail.com>
2023-05-22 13:54:46 -0600

    created user router.

---
+++ routes/api/users.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const usersCtrl = require("../../controllers/user");
+const multer = require("multer");
+const upload = multer();
+
+//public routes
+
+router.post("/signup", upload.single("photo"), usersCtrl.signup);
+router.post("/login", usersCtrl.login);
+router.get("/:username", usersCtrl.profile);
+router.post("/updateBio", usersCtrl.updateBio);
+
+//authentication needed routes
+
+module.exports = router;
