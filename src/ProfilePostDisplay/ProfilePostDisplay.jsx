5d2c302b0531f939f7c55f2cb1a6b546a7d6b4d2
Justin Stowe <justinstowe12@gmail.com>
2023-05-24 12:04:16 -0600

    slapped together skeleton of profile post display.

---
+++ src/ProfilePostDisplay/ProfilePostDisplay.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { card } from "semantic-ui-react";
+import Post from "../components/PostCard/PostCard";
+
+export function ProfilePostDisplay(props) {
+  return <div>Profile Post Display Container</div>;
+}
